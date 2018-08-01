9265e323d7b837cc51e044ca3150f2a5dcd61086
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2018-08-01 12:17:04 +0200

    fix: adds mssing changes; adds compile index.json

--- compile.js
+++ compile.js
@@ -29,10 +29,39 @@ function maybeMkdir (pathString) {
     fs.mkdirSync(path.resolve(__dirname, pathString))
   } catch (err) {}
 }
+function walk (dir, done) {
+  var results = []
+  fs.readdir(dir, function (err, list) {
+    if (err) return done(err)
+    var pending = list.length
+    if (!pending) return done(null, results)
+    list.forEach(function (file) {
+      file = path.resolve(dir, file)
+      fs.stat(file, function (err, stat) {
+        if (err) return done(err)
+        if (stat && stat.isDirectory()) {
+          walk(file, function (err, res) {
+            if (err) return done(err)
+            results = results.concat(res)
+            if (!--pending) done(null, results)
+          })
+        } else {
+          results.push(file.replace(dir, ''))
+          if (!--pending) done(null, results)
+        }
+      })
+    })
+  })
+};
 
 module.exports = (schemas) => {
   maybeMkdir('dist')
   iterate(allSchemas)
+
+  walk(path.resolve(__dirname, 'dist'), (err, result) => {
+    if (err) throw err
+    fs.writeFileSync(path.resolve(__dirname, 'dist/index.json'), JSON.stringify(result, null, 2) + '\n')
+  })
 }
 
 module.exports(allSchemas)
