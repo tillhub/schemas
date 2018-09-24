edea3b32782dd79d34d073ce218507cd89d1d4af
Pattawan Gerlings <pattawan.gerlings@tillhub.de>
2018-09-24 17:31:20 +0200

    fix(schemas): create schema for delivery note pdf

--- test/pdfs/pdf.v0.spec.js
+++ test/pdfs/pdf.v0.spec.js
@@ -18,3 +18,12 @@ test('PDFs: create invoices schema validation', function (t) {
   t.error(error, 'should not get any error')
   t.end()
 })
+
+test('PDFs: create delivery notes schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v0/pdfs').templates.delivery_notes.create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
