36b4ee138ef68ae993a5734ff51b1effdc876621
Quynh <quynh.totuan@gmail.com>
2019-05-23 16:47:55 +0200

    feat(safes): add safes book handler and safes_log_book

--- test/safes_log_book/safes_log_book.v0.spec.js
+++ test/safes_log_book/safes_log_book.v0.spec.js
@@ -1,15 +1,6 @@
 const test = require('tape')
 const validate = require('../helpers/validate-schema')
 
-test('Safes Log Book: create schema validation', function (t) {
-  t.plan(2)
-  const createRequest = require('../../lib/v0/safes_log_book').create.request
-  const { valid, error } = validate(createRequest)
-  t.ok(valid, 'create schema is valid')
-  t.error(error, 'should not get any error')
-  t.end()
-})
-
 test('Safes Log Book: get response schema validation', function (t) {
   t.plan(2)
   const getResponse = require('../../lib/v0/safes_log_book').get.response
