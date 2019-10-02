2cfaf5dcb067e0e804a60cb4955368aa99f2505b
Quynh To Tuan <quynh.totuan@tillhub.de>
2019-10-02 16:51:40 +0200

    feat(register_opening_protocols): expose module path

--- test/register_opening_protocols/register_opening_protocols.v0.spec.js
+++ test/register_opening_protocols/register_opening_protocols.v0.spec.js
@@ -36,3 +36,21 @@ test('Register Opening Protocols: update response schema validation', function (
   t.error(error, 'should not get any error')
   t.end()
 })
+
+test('Register Opening Protocols: "get latest" request schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v0/register_opening_protocols').get.latest.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, '"get latest" request schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Register Opening Protocols: "get latest" response schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../lib/v0/register_opening_protocols').get.latest.response
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, '"get latest" response schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
