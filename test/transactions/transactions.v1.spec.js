829718880fc97e2621dcade60b84e2861b40f1d4
Quynh To Tuan <quynh.totuan@tillhub.de>
2019-10-23 15:11:38 +0200

    test(transactions): add v1_legacy test

--- test/transactions/transactions.v1.spec.js
+++ test/transactions/transactions.v1.spec.js
@@ -1,9 +1,18 @@
 const test = require('tape')
 const validate = require('../helpers/validate-schema')
 
-test('Transactions: create schema validation', function (t) {
+test('Transactions: v1: create schema validation', function (t) {
   t.plan(2)
   const createRequest = require('../../lib/v1/transactions').create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Transactions: v1: legacy: create schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v1/transactions').legacy.create.request
   const { valid, error } = validate(createRequest)
   t.ok(valid, 'create schema is valid')
   t.error(error, 'should not get any error')
