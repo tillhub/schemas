b930d3a575da389cad57b601c5408ac198cb4486
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2019-10-24 00:46:33 +0200

    feat(products): add price book

---
+++ test/products/products.v1.prices.book.js
@@ -0,0 +1,20 @@
+const test = require('tape')
+const validate = require('../helpers/validate-schema')
+
+test('Products V1 price book: create schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v1/products').prices.book.create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Products V1 price book: update schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../lib/v1/products').prices.book.update.request
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
