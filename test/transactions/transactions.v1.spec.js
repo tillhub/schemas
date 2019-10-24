957ae779a5d2a1b010540068a8cf7bcad5a54fb1
Quynh To Tuan <quynh.totuan@tillhub.de>
2019-10-24 16:11:44 +0200

    feat(tx_v1_legacy): add responses

--- test/transactions/transactions.v1.spec.js
+++ test/transactions/transactions.v1.spec.js
@@ -10,9 +10,18 @@ test('Transactions: v1: create schema validation', function (t) {
   t.end()
 })
 
-test('Transactions: v1: legacy: create schema validation', function (t) {
+test('Transactions: v1: legacy: create request schema', function (t) {
   t.plan(2)
   const createRequest = require('../../lib/v1/transactions').legacy.create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Transactions: v1: legacy: create response schema', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v1/transactions').legacy.create.response
   const { valid, error } = validate(createRequest)
   t.ok(valid, 'create schema is valid')
   t.error(error, 'should not get any error')
