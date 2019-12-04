704397f261a4850bf89ba16b3eee6dc5c2b00515
Barbara <barbara.fs@gmail.com>
2019-12-04 16:52:49 +0100

    feat(price_book): add test

--- test/products/products.v1.prices.book.js
+++ test/products/products.v1.prices.book.js
@@ -18,3 +18,21 @@ test('Products V1 price book: update schema validation', function (t) {
   t.error(error, 'should not get any error')
   t.end()
 })
+
+test('Products V1 price book entry: create schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v1/products').prices.book.entry.create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Products V1 price book entry: update schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../lib/v1/products').prices.book.entry.update.request
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
