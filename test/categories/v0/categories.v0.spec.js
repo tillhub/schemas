80bdc7a2c5b9afa92b455e2c207bb55cfb98ab1a
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2019-12-27 18:08:47 +0100

    feat: add categories
    Signed-off-by: Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>

---
+++ test/categories/v0/categories.v0.spec.js
@@ -0,0 +1,21 @@
+
+const test = require('tape')
+const validate = require('../../helpers/validate-schema')
+
+test('Categories V0: create schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../../lib/v0/categories').create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Categories V0: update schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../../lib/v0/categories').update.request
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
