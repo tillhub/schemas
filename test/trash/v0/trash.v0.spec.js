bae3a9965094c410f37cefa3ab31d3e7b0463486
Bárbara Farias <barbara.fs@gmail.com>
2020-01-14 15:13:42 +0100

    Feat(trash): add query schema to trash endpoint (#328)
    * feat(trash): add query schema to trash endpoint
    
    * fix(trash): reorder types
    
    * fix(trash): add more resources

---
+++ test/trash/v0/trash.v0.spec.js
@@ -0,0 +1,21 @@
+
+const test = require('tape')
+const validate = require('../../helpers/validate-schema')
+
+test('Trash V0: create schema validation', function (t) {
+  t.plan(2)
+  const getRequest = require('../../../lib/v0/trash').get.request
+  const { valid, error } = validate(getRequest)
+  t.ok(valid, 'get schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Trash V0: update schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../../lib/v0/trash').update.request
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
