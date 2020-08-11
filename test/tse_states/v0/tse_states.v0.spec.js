fdd2e84d027f1eefe5b0cfeeedd7dfebe71cf946
barbarafarias <barbara.fs@gmail.com>
2020-08-11 14:14:15 +0200

    fix(fiscalisation): remove update test

--- test/tse_states/v0/tse_states.v0.spec.js
+++ test/tse_states/v0/tse_states.v0.spec.js
@@ -10,12 +10,3 @@ test('Tse States V0: create schema validation', function (t) {
   t.error(error, 'should not get any error')
   t.end()
 })
-
-test('Tse States V0: update schema validation', function (t) {
-  t.plan(2)
-  const updateRequest = require('../../../lib/v0/tse_states').update.request
-  const { valid, error } = validate(updateRequest)
-  t.ok(valid, 'update schema is valid')
-  t.error(error, 'should not get any error')
-  t.end()
-})
