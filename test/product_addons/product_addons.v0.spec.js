cac9df6ff55d0ce4469cb6cc118b750f2671323a
Mauro Gri <mauro.gri@tillhub.de>
2021-07-21 12:00:29 +0200

    feat(addons): add test

---
+++ test/product_addons/product_addons.v0.spec.js
@@ -0,0 +1,20 @@
+const test = require('tape')
+const validate = require('../helpers/validate-schema')
+
+test('Product Addons V0: create schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v0/product_addons').create
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('Product Addons V0: update schema validation', function (t) {
+  t.plan(2)
+  const updateRequest = require('../../lib/v0/product_addons').update
+  const { valid, error } = validate(updateRequest)
+  t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
