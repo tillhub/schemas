46fd51f3014c5bd027996f2949c8fd04757ede8c
Mauro Gri <mauro.gri@tillhub.de>
2021-07-21 13:52:35 +0200

    feat(addons): fix test

--- test/product_addons/product_addons.v0.spec.js
+++ test/product_addons/product_addons.v0.spec.js
@@ -3,7 +3,7 @@ const validate = require('../helpers/validate-schema')
 
 test('Product Addons V0: create schema validation', function (t) {
   t.plan(2)
-  const createRequest = require('../../lib/v0/product_addons').create
+  const createRequest = require('../../lib/v0/product_addons').create.request
   const { valid, error } = validate(createRequest)
   t.ok(valid, 'create schema is valid')
   t.error(error, 'should not get any error')
@@ -12,7 +12,7 @@ test('Product Addons V0: create schema validation', function (t) {
 
 test('Product Addons V0: update schema validation', function (t) {
   t.plan(2)
-  const updateRequest = require('../../lib/v0/product_addons').update
+  const updateRequest = require('../../lib/v0/product_addons').update.request
   const { valid, error } = validate(updateRequest)
   t.ok(valid, 'update schema is valid')
   t.error(error, 'should not get any error')
