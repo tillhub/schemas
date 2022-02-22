774c03b064c74b9f1a092c6aee2ff5ca8d27d634
Aleksey Maltsev <aleksey.a.maltsev@gmail.com>
2022-02-22 18:38:22 +0100

    feat(carts): add detailed schema #API-1394 (#673)
    feat(carts): add detailed schema #API-1394

--- test/carts/carts.v1.spec.js
+++ test/carts/carts.v1.spec.js
@@ -1,11 +1,51 @@
 const test = require('tape')
 const validate = require('../helpers/validate-schema')
+const cartsSchema = require('../../lib/v1/carts')
 
-test('Carts: create schema validation', function (t) {
-  t.plan(2)
-  const createRequest = require('../../lib/v1/carts').create.request
-  const { valid, error } = validate(createRequest)
-  t.ok(valid, 'create schema is valid')
-  t.error(error, 'should not get any error')
+test('Carts:v1: create schema validation', function (t) {
+  {
+    const { valid, error } = validate(cartsSchema.create.request)
+    t.ok(valid, 'create request schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
+  {
+    const { valid, error } = validate(cartsSchema.create.response)
+    t.ok(valid, 'create response schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
+  t.end()
+})
+
+test('Carts:v1: read schema validation', function (t) {
+  {
+    const { valid, error } = validate(cartsSchema.read.all.response)
+    t.ok(valid, 'read.all response schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
+  {
+    const { valid, error } = validate(cartsSchema.read.one.response)
+    t.ok(valid, 'read.one response schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
+  t.end()
+})
+
+test('Carts:v1: patch schema validation', function (t) {
+  {
+    const { valid, error } = validate(cartsSchema.patch.request)
+    t.ok(valid, 'patch request schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
+  {
+    const { valid, error } = validate(cartsSchema.patch.response)
+    t.ok(valid, 'patch response schema is valid')
+    t.error(error, 'should not get any error')
+  }
+
   t.end()
 })
