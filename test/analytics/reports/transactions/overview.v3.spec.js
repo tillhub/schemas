a5387adec399e9d7493d58b81ae1d589c81e8af9
Aleksey Maltsev <aleksey.a.maltsev@gmail.com>
2022-03-11 13:10:13 +0100

    feat(analytics): fix test name #API-1042

--- test/analytics/reports/transactions/overview.v3.spec.js
+++ test/analytics/reports/transactions/overview.v3.spec.js
@@ -2,7 +2,7 @@ const test = require('tape')
 const validate = require('../../../helpers/validate-schema')
 const overviewSchema = require('../../../../lib/v3/analytics/reports/transactions/overview')
 
-test('Analytics:reports:transactions:overview:v1: response schema validation', function (t) {
+test('analytics:reports:transactions:overview:v3: response schema validation', function (t) {
   const { valid, error } = validate(overviewSchema.response)
   t.ok(valid, 'create response schema is valid')
   t.error(error, 'should not get any error')
