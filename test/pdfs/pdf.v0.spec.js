ebb0123ac51dc8e6e1277849f148058c88db659e
Evgenii Strigo <evgenii.strigo@tillhub.de>
2023-06-11 16:17:33 +0300

    feat(purchase-orders): Purchase order pdf generation schema

--- test/pdfs/pdf.v0.spec.js
+++ test/pdfs/pdf.v0.spec.js
@@ -19,6 +19,15 @@ test('PDFs: create invoices schema validation', function (t) {
   t.end()
 })
 
+test('PDFs: create purchase orders schema validation', function (t) {
+  t.plan(2)
+  const createRequest = require('../../lib/v0/pdfs').templates.purchase_orders.create.request
+  const { valid, error } = validate(createRequest)
+  t.ok(valid, 'create schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
 test('PDFs: create delivery notes schema validation', function (t) {
   t.plan(2)
   const createRequest = require('../../lib/v0/pdfs').templates.delivery_notes.create.request
