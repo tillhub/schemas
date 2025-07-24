cc2241521e9d53faac376da119f3ee3c44866c21
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2025-07-24 12:53:10 +0300

    feat(UNTIL-12873): add stricter validation for user scopes

--- test/user_permission_templates/v0/user_permission_templates.v0.spec.js
+++ test/user_permission_templates/v0/user_permission_templates.v0.spec.js
@@ -1,4 +1,3 @@
-
 const test = require('tape')
 const validate = require('../../helpers/validate-schema')
 
@@ -17,5 +16,50 @@ test('User Permission Templates V0: update schema validation', function (t) {
   const { valid, error } = validate(updateRequest)
   t.ok(valid, 'update schema is valid')
   t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('User Permission Templates V0: scopes validation - valid enum', function (t) {
+  const createRequest = require('../../../lib/v0/user_permission_templates').create.request
+  const validate = require('../../helpers/validate-schema')
+  const validPayload = {
+    name: 'Test Template',
+    scopes: [require('../../../lib/common/permissions').staff.items.enum[0]],
+    active: true,
+    deleted: false
+  }
+  const { valid, error } = validate(createRequest, validPayload)
+  t.ok(valid, 'should accept valid enum value in scopes')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('User Permission Templates V0: scopes validation - arbitrary string (backward compatible)', function (t) {
+  const createRequest = require('../../../lib/v0/user_permission_templates').create.request
+  const validate = require('../../helpers/validate-schema')
+  const validPayload = {
+    name: 'Test Template',
+    scopes: ['some:custom:permission'],
+    active: true,
+    deleted: false
+  }
+  const { valid, error } = validate(createRequest, validPayload)
+  t.ok(valid, 'should accept arbitrary string in scopes for backward compatibility')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('User Permission Templates V0: scopes validation - invalid type', function (t) {
+  const createRequest = require('../../../lib/v0/user_permission_templates').create.request
+  const validate = require('../../helpers/validate-schema')
+  const invalidPayload = {
+    name: 'Test Template',
+    scopes: [123],
+    active: true,
+    deleted: false
+  }
+  const { valid, error } = validate(createRequest, invalidPayload)
+  t.notOk(valid, 'should reject non-string values in scopes')
+  t.ok(error, 'should get an error')
   t.end()
 })
