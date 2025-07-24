9eb8c77eef44514fcdea1a4f50f7d633000551a8
Maks DOVHOPOLYI <dovgopolyy0000@gmail.com>
2025-07-24 14:27:19 +0300

    feat(UNTIL-12873): add stricter validation for user scopes (#1079)

--- test/user_permission_templates/v0/user_permission_templates.v0.spec.js
+++ test/user_permission_templates/v0/user_permission_templates.v0.spec.js
@@ -1,4 +1,3 @@
-
 const test = require('tape')
 const validate = require('../../helpers/validate-schema')
 
@@ -16,6 +15,36 @@ test('User Permission Templates V0: update schema validation', function (t) {
   const updateRequest = require('../../../lib/v0/user_permission_templates').update.request
   const { valid, error } = validate(updateRequest)
   t.ok(valid, 'update schema is valid')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('User Permission Templates V0: scopes validation - valid enum', function (t) {
+  const createRequest = require('../../../lib/v0/user_permission_templates').create.request
+  const validate = require('../../helpers/validate-schema')
+  const validPayload = {
+    name: 'Test Template',
+    scopes: [require('../../../lib/common/permissions').staff.items.enum[0]],
+    active: true,
+    deleted: false
+  }
+  const { valid, error } = validate(createRequest, validPayload)
+  t.ok(valid, 'should accept valid enum value in scopes')
+  t.error(error, 'should not get any error')
+  t.end()
+})
+
+test('User Permission Templates V0: scopes validation - arbitrary string (backward compatible)', function (t) {
+  const createRequest = require('../../../lib/v0/user_permission_templates').create.request
+  const validate = require('../../helpers/validate-schema')
+  const validPayload = {
+    name: 'Test Template',
+    scopes: ['some:custom:permission'],
+    active: true,
+    deleted: false
+  }
+  const { valid, error } = validate(createRequest, validPayload)
+  t.ok(valid, 'should accept arbitrary string in scopes for backward compatibility')
   t.error(error, 'should not get any error')
   t.end()
 })
